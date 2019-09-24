ff31a99933572ae1258250c8afcdc44d7f989f57
juan david <juandavid@keysoftware.com.co>
2019-09-24 14:52:42 -0500

    Update Uab.m

--- Uab.m
+++ Uab.m
@@ -1,19 +1,20 @@
 function matrix = Uab(mu,x,y,z)
 %Uab returns matrix 
 %   Uab is the symmetric matrix composed of second partial derivatives of U with 
-%   respect to the third body’s position evaluated along the orbit
+%   respect to the third bodyâ€™s position evaluated along the orbit
 
-matrix(1,1) = (mu-1)/((mu+x)^2 + y^2 + z^2)^(3/2) - mu/((mu+x-1)^2 + y^2 + z^2)^(3/2) + (3*mu*(2*mu+2*x-2)*(mu+x-1))/(2*((mu+x-1)^2 + y^2 + z^2)^(5/2)) - (3*(2*mu+2*x)*(mu+x)*(mu-1))/(2*((mu+x)^2 + y^2 + z^2)^(5/2)) + 1;
-matrix(1,2) = (3*mu*y*(mu+x-1))/((mu+x-1)^2 + y^2 + z^2)^(5/2) - (3*y*(mu+x)*(mu-1))/((mu+x)^2 + y^2 + z^2)^(5/2);
-%matrix(1,3) = (3*mu*z*(mu+x-1))/((mu+x-1)^2 + y^2 + z^2)^(5/2) - (3*z*(mu+x)*(mu-1))/((mu+x)^2 + y^2 + z^2)^(5/2);
+d = sqrt((x+mu)^2+y^2+z^2);
+r = sqrt((x - 1 + mu)^2+y^2+z^2);
 
-matrix(2,1) = (3*mu*y*(2*mu+2*x-2))/(2*((mu+x-1)^2 + y^2 + z^2)^(5/2)) - (3*y*(2*mu+2*x)*(mu-1))/(2*((mu+x)^2 + y^2 + z^2)^(5/2));
-matrix(2,2) = (mu-1)/((mu+x)^2 + y^2 + z^2)^(3/2) - mu/((mu+x-1)^2 + y^2 + z^2)^(3/2) - (3*y^2*(mu-1))/((mu+x)^2 + y^2 + z^2)^(5/2) + (3*mu*y^2)/((mu+x-1)^2 + y^2 + z^2)^(5/2) + 1;
-%matrix(2,3) = (3*mu*y*z)/((mu+x-1)^2 + y^2 + z^2)^(5/2) - (3*y*z*(mu-1))/((mu+x)^2 + y^2 + z^2)^(5/2);
+Uxx = 1 - (1-mu)/d^3 - mu/r^3 + (3*(1-mu)*(x+mu)^2)/d^5 + (3*mu*(x-1+mu)^2)/r^5;
+Uxy = (3*(1-mu)*(x+mu)*y)/d^5 + (3*mu*(x-1+mu)*y)/r^5;
+Uyy = 1 - (1-mu)/d^3 - mu/r^3 + (3*(1-mu)*y^2)/d^5 + (3*mu*y^2)/r^5;
 
-%matrix(3,1) = (3*mu*z*(2*mu + 2*x - 2))/(2*((mu + x - 1)^2 + y^2 + z^2)^(5/2)) - (3*z*(2*mu + 2*x)*(mu - 1))/(2*((mu + x)^2 + y^2 + z^2)^(5/2));
-%matrix(3,2) = (3*mu*y*z)/((mu + x - 1)^2 + y^2 + z^2)^(5/2) - (3*y*z*(mu - 1))/((mu + x)^2 + y^2 + z^2)^(5/2);
-%matrix(3,3) = (mu - 1)/((mu + x)^2 + y^2 + z^2)^(3/2) - mu/((mu + x - 1)^2 + y^2 + z^2)^(3/2) - (3*z^2*(mu - 1))/((mu + x)^2 + y^2 + z^2)^(5/2) + (3*mu*z^2)/((mu + x - 1)^2 + y^2 + z^2)^(5/2);
+matrix(1,1) = Uxx; 
+matrix(1,2) = Uxy;
+
+matrix(2,1) = Uxy;
+matrix(2,2) = Uyy;
 
 end
 
