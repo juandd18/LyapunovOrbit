e235e54fb50fbb1ab870023d13cdf7d0e5c6ee83
juan david <juandavid@keysoftware.com.co>
2019-09-24 14:53:33 -0500

    Update initialPositionLyapunov.m

--- initialPositionLyapunov.m
+++ initialPositionLyapunov.m
@@ -10,7 +10,7 @@ u_tilda = u*(abs(Xe-1+u))^(-3) + (1-u)*(abs(Xe+u))^(-3);
 
 % equation pag. 46 section 2.7
 alpha2 = (1/2)*(u_tilda - 2 - sqrt(9*u_tilda^2 - 8*u_tilda)); 
-v = sqrt(-alpha2);
+v = alpha2;
 
 % equation (2.7.2) or pag. 109
 tau = -(v^2 + 2*u_tilda + 1)/(2*v);
